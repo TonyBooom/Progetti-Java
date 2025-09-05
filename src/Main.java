f5ff6fbba7dbbcd727a3169e4fe58ddbe86104be
Antonio <antonio-9708@outlook.it>
2025-09-05 17:22:16 +0200

    Sto provando git, primo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Primo commit");
+        System.out.printf("Primo commita" );
+        System.out.println("Infatti cambia");
     }
+
+    public void prova(){System.out.println("prova");}
+
+    public void prova2(){System.out.println("prova2");}
+
 }
