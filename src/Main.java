a49bb76f014c84e5f23bc1c6d5502157b66c6bc2
Antonio <antonio-9708@outlook.it>
2025-09-05 17:32:58 +0200

    Siamo arrivati

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao mondo!");
-        System.out.println("Siamo arrivati!");
+        System.out.println("WEEE arrivati!");
+
     }
 }
