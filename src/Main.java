803c7a55af2cb32ea1ddcce7cc18921c5c78d643
Antonio <antonio-9708@outlook.it>
2025-09-05 17:40:28 +0200

    Siamo arrivati

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao mondo!");
-        System.out.println("WEEE arrivati!");
-
+        System.out.println("Ripetizione variabili java");
+        int a = 5;
+        double b = 19.99;
+        Boolean c = true;
+        char h = 'A';
+        String d = "Ciao a tutti";
+        System.out.println("Valore di a: " + a);
+        System.out.println("Valore di b: " + b);
+        System.out.println("Valore di c: " + c);
+        System.out.println("Valore di h: " + h);
+        System.out.println("Valore di d: " + d);
     }
 }
